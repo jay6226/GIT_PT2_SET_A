2597ede1af9fadcb665701c6374b34cea857c779
gddite <89434981+gddite@users.noreply.github.com>
2021-10-12 18:58:53 +0800

    Add files via upload

---
+++ _Obstacles/ObstacleSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+    public GameObject[] obstacle;
+
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(TimedSpawner(1));
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private IEnumerator TimedSpawner(int Timer)
+    {
+        while (true) { 
+        Instantiate(obstacle[Random.Range(0, obstacle.Length)], transform.position = new Vector3(Random.Range(-3, 3), 0, 6), Quaternion.identity);
+        yield return new WaitForSeconds(Timer);
+        }
+    }
+}
